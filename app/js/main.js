1b92e472e296358217015a5f8179f1ef713e3345
Dimas <zhuravel16@gmail.com>
2019-09-22 12:18:45 +0300

    add header and slider

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+
+
+    $('.slick_slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        infinite: false
+    });
+
+
+
+
+
+
+
+});
